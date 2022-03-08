f2bebe04c6f46ecb35ea6ac1d83d509d1cc12ed2
Héber Lima Silva <heberlimasilva@gmail.com>
2022-03-08 14:09:03 -0300

    Criando tela de listagem de usuários

---
+++ src/pages/users/index.tsx
@@ -0,0 +1,58 @@
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, Text } from "@chakra-ui/react";
+import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import { Header } from "../../components/Header";
+import { Sidebar } from "../../components/Sidebar";
+
+export default function UserList(){
+    return(
+        <Box>
+            <Header/>
+            <Flex width="100%" my="6" maxW={1480} mx="auto" px="6">
+                <Sidebar />
+
+                <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+                    <Flex mb="8" justify="space-between" align="center">
+                        <Heading size="lg" fontWeight="normal">Usuários</Heading>
+                        <Button as="a" size="sm" fontSize="small" colorScheme="pink" leftIcon={<Icon as={RiAddLine} fontSize="20"/>} >
+                            Criar novo
+                        </Button>
+                    </Flex>
+
+                    <Table colorScheme="whiteAlpha">
+                        <Thead>
+                            <Tr>
+                                <Th px="6" color="gray.300" w="8">
+                                    <Checkbox colorScheme="pink"/>
+                                </Th>
+                                <Th>Usuário</Th>
+                                <Th>Data de Cadastro</Th>
+                                <Th w="8"></Th>
+                            </Tr>
+                        </Thead>
+                        <Tbody>
+                            <Tr>
+                                <Td>
+                                    <Checkbox colorScheme="pink"/>
+                                </Td>
+                                <Td>
+                                    <Box>
+                                        <Text fontWeight="bold">Héber Silva</Text>
+                                        <Text fontSize="small" color="gray.300">heberlimasilva@gmail.com</Text>
+                                    </Box>
+                                </Td>
+                                <Td>08 de março de 2022</Td>
+                                <Td>
+                                    <Button as="a" size="sm" fontSize="small" colorScheme="purple" leftIcon={<Icon as={RiPencilLine} fontSize="16"/> }>
+                                        Editar
+                                    </Button>
+                                </Td>
+                            </Tr>
+                        </Tbody>
+                    </Table>
+
+
+                </Box>
+            </Flex>
+        </Box>
+    )
+}
