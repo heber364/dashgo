c7195f6b4294c71dfe2860417654c2efcbda8e3b
Héber Lima Silva <heberlimasilva@gmail.com>
2022-03-19 12:15:34 -0300

    Adicionando a lib react-hook-form, e iniciando o form de autenticação

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -1,28 +1,40 @@
-import { Input as ChakraInput, FormLabel, FormControl, InputProps as ChakraInputProps} from  '@chakra-ui/react'
+import { forwardRef, ForwardRefRenderFunction } from "react";
+
+import {
+  Input as ChakraInput,
+  FormLabel,
+  FormControl,
+  InputProps as ChakraInputProps,
+} from "@chakra-ui/react";
 
 interface InputProps extends ChakraInputProps {
-    name: string;
-    label?: string;
+  name: string;
+  label?: string;
 }
 
+const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = (
+  { name, label, ...rest },
+  ref
+) => {
+  return (
+    <FormControl>
+      {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
+      <ChakraInput
+        name={name}
+        id={name}
+        type={"email"}
+        focusBorderColor="pink.500"
+        bg="gray.900"
+        variant="filled"
+        _hover={{
+          bgColor: "gray.900",
+        }}
+        size="lg"
+        ref={ref}
+        {...rest}
+      />
+    </FormControl>
+  );
+};
 
-export function Input({name, label, ...rest} : InputProps){
-    return(
-        <FormControl>
-            { !! label && <FormLabel htmlFor={name}>{label}</FormLabel>}
-            <ChakraInput
-              name={name}
-              id={name}
-              type={"email"} 
-              focusBorderColor='pink.500'
-              bg="gray.900"
-              variant="filled"
-              _hover={{
-                bgColor: 'gray.900'
-              }}
-              size="lg"
-              {...rest}
-            />
-          </FormControl>
-    );
-}
+export const Input = forwardRef(InputBase);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,22 @@
 import { Flex, Button , Stack} from  '@chakra-ui/react'
+import { SubmitHandler, useForm } from 'react-hook-form'
 import { Input } from '../components/Form/Input'
 
+
+type SignFormData = {
+  email: string,
+  password: string
+}
+
 export default function Home() {
+  const { register, handleSubmit, formState } = useForm()
+  
+  const handleSign: SubmitHandler<SignFormData> = async (values) =>{
+    await new Promise(resolve => setTimeout(resolve,1000))
+
+    console.log(values)
+  }
+
   return (
    <Flex
      w="100vw"
@@ -17,12 +32,13 @@ export default function Home() {
         p="8"
         borderRadius={8}
         flexDir="column"
+        onSubmit={handleSubmit(handleSign)}
       >
         <Stack spacing='4'>
-          <Input type="email" name='email' label='E-mail'/>
-          <Input type="password" name='password' label='Password'/>
+          <Input type="email" name='email' label='E-mail' {...register('email')}/>
+          <Input type="password" name='password' label='Password' {...register('password')}/>
         </Stack>
-        <Button type="submit" mt="6" colorScheme="pink" size='lg'>Entrar</Button>
+        <Button type="submit" mt="6" colorScheme="pink" size='lg' isLoading={formState.isSubmitting}>Entrar</Button>
       </Flex>
     </Flex>
   )
