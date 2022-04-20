540b5e21ec798617bdf35b03687a024093a35746
Héber Lima Silva <heberlimasilva@gmail.com>
2022-04-20 19:54:06 -0300

    Finalizando configuração da paginação

--- src/components/Pagination/PaginationItem.tsx
+++ src/components/Pagination/PaginationItem.tsx
@@ -2,11 +2,14 @@ import { Button } from "@chakra-ui/react";
 
 interface PaginationItemProps {
     number: number;
-    isCurrent?: boolean;
+    isCurrent?: boolean;  
+    onPageChange: (page: number) => void;
+
 }
 export function PaginationItem({
     isCurrent = false, 
-    number
+    number, 
+    onPageChange
 } : PaginationItemProps){
     
     if(isCurrent){
@@ -36,6 +39,7 @@ export function PaginationItem({
           _hover={{
             bg: "gray.500",
           }}
+          onClick={() => onPageChange(number)}
         >
           {number}
         </Button>
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -21,11 +21,12 @@ import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useUsers } from "../../services/hooks/useUsers";
 
 export default function UserList() {
-  const { data, isLoading, isFetching, error } = useUsers()
+  const [ page, setPage] = useState(1);
+  const { data, isLoading, isFetching, error } = useUsers(page)
 
   const isWideVersion = useBreakpointValue({
     base: false,
@@ -81,7 +82,7 @@ export default function UserList() {
                   </Tr>
                 </Thead>
                 <Tbody>
-                  {data.map((user) => {
+                  {data.users.map((user) => {
                     return (
                       <Tr key={user.id}>
                         <Td px={["4", "4", "6"]}>
@@ -117,9 +118,9 @@ export default function UserList() {
                 </Tbody>
               </Table>
               <Pagination 
-                totalCountOfRegisters={200}
-                currentPage={5}
-                onPageChange={() => {}}
+                totalCountOfRegisters={data.totalCount}
+                currentPage={page}
+                onPageChange={setPage}
               />
             </>
           )}
